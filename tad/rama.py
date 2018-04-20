be8b64ba8fc8c1709fe8b98af293701e08be9aae
Andres Villagra <andres_vf@icloud.com>
2018-04-20 17:22:23 -0300

    Documentación.

--- tad/rama.py
+++ tad/rama.py
@@ -1,14 +1,24 @@
 
 class Rama:
     def __init__(self, numero_elementos):
+        '''
+        :param numero_elementos: Cantidad de ramas que tendra cuando crezca.
+        '''
         self.numero_elementos = numero_elementos
         self.siguientes = []
 
     def crecer(self):
+        '''
+        Crear nuevas ramas segun el número de elementos.
+        Si las ramas ya existen, pasa la llamada a ellas para que estas crezcan.
+        '''
         for _ in range(self.numero_elementos):
             self.siguientes.append(Rama(self.numero_elementos))
 
     def __str__(self):
+        '''
+        :return: Una representación en texto de la estructura del árbol.
+        '''
         my_string = "R("
         if self.siguientes:
             for sig in self.siguientes:
